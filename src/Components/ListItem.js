0dc54869d7094fb0532613bec4132ef01f9aa76e
Sai Siddharth Tadepalli <9sidharth96@gmail.com>
2017-09-17 19:27:17 +0530

    Added Basic Touch Functionality

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -1,23 +1,45 @@
-import React , {Component} from 'react';
-import {Text, ScrollView, View} from 'react-native';
-import CardSection from './Common/CardSection';
+import React, { Component } from "react";
+import { Text, TouchableWithoutFeedback, View } from "react-native";
+import { connect } from "react-redux";
+import CardSection from "./Common/CardSection";
+import * as actions from "../actions";
 
- class ListItem extends Component {
-   render(){
-     const {titleStyling} = Styles;
-     return(
-         <CardSection>
-            <Text style = {titleStyling}>{this.props.library.title}</Text>
-         </CardSection>
-     );
-   }
- }
+class ListItem extends Component {
+  renderDescription() {
+    const { library, selectedLibraryId } = this.props;
+
+    if (library.id === selectedLibraryId) {
+    return (<Text>{library.description}</Text>);
+    }
+  }
+
+  render() {
+    const { titleStyling } = Styles;
+    const { id, title } = this.props.library;
+
+    return (
+      <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+        <View>
+          <CardSection>
+            <Text style={titleStyling}>{title}</Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
 
 const Styles = {
-  titleStyling : {
-    fontSize : 18,
-    color : "#061D32",
-    paddingLeft : 30
+  titleStyling: {
+    fontSize: 18,
+    color: "#061D32",
+    paddingLeft: 30
   }
 };
- export default ListItem;
+
+const mapStateToProps = state => {
+  return { selectedLibraryId: state.selectedLibraryId };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
