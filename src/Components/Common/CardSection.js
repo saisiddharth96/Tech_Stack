8e160779347d3340abcf722aef41e134aaad806b
Sai Siddharth Tadepalli <9sidharth96@gmail.com>
2017-09-17 21:34:34 +0530

    Added styling and animation

--- src/Components/Common/CardSection.js
+++ src/Components/Common/CardSection.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import {View} from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
-const CardSection = (props) => {
-  return (
-    <View style = {Style.containerStyle} >
-    {props.children}
-    </View>
-  );
+const CardSection = props => {
+  return <View style={Style.containerStyle}>{props.children}</View>;
 };
 
 const Style = {
-    containerStyle : {
-        borderBottomWidth : 1,
-        padding : 10,
-        backgroundColor : '#fff',
-        justifyContent : 'flex-start',
-        flexDirection : 'row',
-        position : 'relative'
-    }
+  containerStyle: {
+    borderBottomWidth: 1,
+    height: 40,
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    position: "relative"
+  }
 };
 
 export default CardSection;
--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -1,45 +1,80 @@
 import React, { Component } from "react";
-import { Text, TouchableWithoutFeedback, View } from "react-native";
+import { Text, 
+  NativeModules,
+  TouchableWithoutFeedback,
+   View,
+  LayoutAnimation } from "react-native";
 import { connect } from "react-redux";
 import CardSection from "./Common/CardSection";
 import * as actions from "../actions";
 
-class ListItem extends Component {
-  renderDescription() {
-    const { library, selectedLibraryId } = this.props;
+const { UIManager } = NativeModules;
 
-    if (library.id === selectedLibraryId) {
-    return (<Text>{library.description}</Text>);
+UIManager.setLayoutAnimationEnabledExperimental &&
+UIManager.setLayoutAnimationEnabledExperimental(true);
+
+
+class ListItem extends Component {
+  componentWillUpdate(){
+    LayoutAnimation.spring();
+  }
+  renderDescription() {
+    const { titleStyling, textStyling } = Styles;
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <View>
+          <Text style={textStyling}>{library.description}</Text>
+        </View>
+      );
     }
   }
 
   render() {
-    const { titleStyling } = Styles;
+    const { titleStyling, textStyling } = Styles;
     const { id, title } = this.props.library;
 
     return (
-      <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
-        <View>
-          <CardSection>
-            <Text style={titleStyling}>{title}</Text>
-          </CardSection>
-          {this.renderDescription()}
-        </View>
-      </TouchableWithoutFeedback>
+    
+        <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+          <View style={{ overflow : 'hidden' }}>
+            <CardSection>
+              <Text style={titleStyling}>{title}</Text>
+            </CardSection>
+            {this.renderDescription()}
+          </View>
+        </TouchableWithoutFeedback>
+      
     );
   }
 }
 
 const Styles = {
   titleStyling: {
+    flex: 1,
     fontSize: 18,
+    color: "#F4FBFF",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 20,
+    paddingTop: 3,
+    backgroundColor: "#0D5B8F"
+  },
+  textStyling: {
+    fontSize: 16,
     color: "#061D32",
-    paddingLeft: 30
+    flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    backgroundColor: "#E4F2FF",
+    padding: 10
   }
 };
 
-const mapStateToProps = state => {
-  return { selectedLibraryId: state.selectedLibraryId };
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded };
 };
 
 export default connect(mapStateToProps, actions)(ListItem);
